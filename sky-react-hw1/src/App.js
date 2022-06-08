6702aaf1617a960de27189780ceb4bc89b22c8e6
Yarengina <yarenginalyubava@gmail.com>
2022-06-08 18:24:42 +0300

    rewritten to JSX

--- sky-react-hw1/src/App.js
+++ sky-react-hw1/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg'
 import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Book(props) {
+    return React.createElement('div', {}, [
+      React.createElement('h2', {}, props.name),
+      React.createElement('p', {}, props.year),
+      React.createElement('p', {}, props.price)
+    ])
+  }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <h1 id='hello' className='class1'>Hello from React</h1>
+            <Book name='JS for beginners' year='2018' price='1000' />
+            <Book name='React' year='2020' price='1200' />
+            <Book name='Vue JS' year='2019' price='1100' />
+        </div>
+    )
 }
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 
+  rootElement
+)
 export default App
